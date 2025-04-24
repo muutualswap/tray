0196149192eb839189b3b8da1f458363d4f8d116
muutualswap <75495839+muutualswap@users.noreply.github.com>
2025-04-24 17:36:46 -0400

    Update type.ts

--- public/tradingview/type.ts
+++ public/tradingview/type.ts
@@ -1,4 +1,4 @@
-import { LibrarySymbolInfo } from '@/charting_library/charting_library'
+import { LibrarySymbolInfo } from 'public/tradingview/charting_library'
 
 export interface SymbolInfo extends LibrarySymbolInfo {
   poolId: string
