5ec72395732bd2b24f3854cad8c58f9f0af2a3bb
CorpeAI <35112540+corpeai@users.noreply.github.com>
2025-04-24 16:45:14 -0400

    Update TVChart.tsx

--- src/components/TradeView/TVChart.tsx
+++ src/components/TradeView/TVChart.tsx
@@ -7,7 +7,7 @@ import {
   ChartPropertiesOverrides,
   Timezone,
   Bar
-} from '@/charting_library'
+} from 'publc/tradingview/charting_library'
 import { useEffect, useMemo, useState } from 'react'
 import { Themes, THEME_NAMES, AppTheme, AppColorMode } from './TvTheme'
 import { Box, useColorMode } from '@chakra-ui/react'
